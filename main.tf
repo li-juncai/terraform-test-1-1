9362036b429ae8647557644a99382fdb37e54fcc
li-juncai <1844287927@qq.com>
2025-11-27 16:31:20 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,26 @@
-provider "alicloud" {
-  region = "cn-shanghai"
-  access_key = var.access_key
-  secret_key = var.secret_key
+terraform {
+  required_providers {
+     alicloud = {
+       source  = "aliyun/alicloud"
+       version = "~> 1.200"
+     }
+  }
+  backend "remote" {  # 使用Terraform Cloud/Enterprise远程状态存储
+     organization = "ljc-test-1"
+     workspaces {
+       name = "workspace-1-1"
+     }
+   }
 }
 
-resource "alicloud_ram_role" "test_role" {
+provider "alicloud" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = "cn-shanghai"
+}
+
+resource "alicloud_ram_role" "example" {
   name        = "tf-test-role"
-  description = "Created by Terraform without permissions"
+  description  = "Created by Terraform GitHub Actions"
   force       = true
 }
