f97e8dc1a143ebaa12e53adf94ebe5f70754d07f
li-juncai <1844287927@qq.com>
2025-11-27 14:54:21 +0800

    Update variables.tf
    test1.1

--- variables.tf
+++ variables.tf
@@ -1,11 +1,8 @@
-variable "instance_name" {
-  description = "Value of the EC2 instance's Name tag."
-  type        = string
-  default     = "learn-terraform"
+variable "alicloud_access_key" {
+  type      = string
+  sensitive = true
 }
-
-variable "instance_type" {
-  description = "The EC2 instance's type."
-  type        = string
-  default     = "t2.micro"
+variable "alicloud_secret_key" {
+  type      = string
+  sensitive = true
 }
